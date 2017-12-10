7272304b60f07c0441a35bf353bec76bd989ca49
phroste <stevenhhui@gmail.com>
2017-12-10 13:39:41 -0500

    added game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 // console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree= "queen";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "king";
+// var cardThree= "queen";
+// var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardFour);
+
+var cards = ['queen', 'king', 'queen', 'king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardOne);
+
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	}
+	else {
+		alert('Sorry, try again.');
+	}
+}
